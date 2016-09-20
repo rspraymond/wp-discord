1e545604ea0492d6b8d5409658aff37c1e8bad32
Raymond Perez <raymond.perez@schomp.com>
2016-09-19 19:22:44 -0600

    AdminFormBuilder class.

---
+++ wp-discord/admin/class-admin-form-field-builder.php
@@ -0,0 +1,80 @@
+<?php
+
+class AdminFormFieldBuilder
+{
+
+    public $name, $type = 'text', $value = null, $attr = [], $placeholder = null;
+
+    /**
+     * AdminFormFieldBuilder constructor.
+     * @param array $params['name', 'type', 'value', 'attr']
+     */
+    public function __construct($params = [])
+    {
+        foreach ($params as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function render()
+    {
+        $field_function = $this->type . '_field_render';
+
+        $output = $this->$field_function();
+
+        echo $output;
+    }
+
+    public function email_field_render()
+    {
+        return $this->text_field_render('email');
+    }
+
+    public function checkbox_field_render()
+    {
+        //@TODO checkbox_field_render
+    }
+
+    public function number_field_render()
+    {
+        return $this->text_field_render('number');
+    }
+
+    public function radio_field_render()
+    {
+        //@TODO radio_field_render
+    }
+
+    public function select_field_render()
+    {
+        //@TODO select_field_render
+    }
+
+    public function text_field_render($type = 'text')
+    {
+        $text_field = '<input type="' . $type . '" value="' . $this->value . '" name="' . $this->name . '"';
+
+        if (!empty($this->attr)) {
+            foreach ($this->attr as $key => $value) {
+                $text_field .= ' ' . $key . '="' . $value . '"';
+            }
+        }
+
+        $text_field .= '>'; // End of tag
+
+        return $text_field;
+    }
+
+    public function textarea_field_render()
+    {
+        //@TODO textarea_field_render
+    }
+
+    public static function render_field($params) {
+        $field = new self($params);
+
+        $field->render();
+    }
+
+
+}
--- wp-discord/admin/class-settings-tab.php
+++ wp-discord/admin/class-settings-tab.php
@@ -1,27 +1,37 @@
 <?php
 
+include_once(plugin_dir_path(__FILE__) . 'class-admin-form-field-builder.php');
+
 class SettingsTab
 {
-    public $partial, $name, $params = [];
+    public $partial, $name, $fields = [], $params = [];
 
     public function __construct($name, $partial = null, $params = [])
     {
         $this->name = $name;
 
         if (empty($partial)) {
-            $this->partial =  $this->get_partial_path();
+            $this->partial = $this->get_partial_path();
         } else {
             $this->partial = $partial;
         }
 
         $this->params = $params;
+
+        if (isset($this->params['fields'])) {
+            $this->fields = $this->params['fields'];
+        }
+
+        $this->set_fields();
     }
 
     public function display()
     {
         $this->set_params();
 
+        echo '<div class="postbox"><div class="inside">';
         include $this->partial;
+        echo '</div></div>';
     }
 
     public function get_display_name()
@@ -34,9 +44,23 @@ class SettingsTab
         return plugin_dir_path(__FILE__) . 'partials/' . $this->name . '_tab.php';
     }
 
+    public function set_fields()
+    {
+        //check fields settings
+        $fields_function_name = $this->name . '_fields';
+
+        if (method_exists($this, $fields_function_name)) {
+            $this->fields = $this->$fields_function_name();
+        }
+
+        foreach ($this->fields as $key => $params) {
+            $this->fields[$key] = new AdminFormFieldBuilder($params);
+        }
+    }
+
     public function set_params()
     {
-        foreach($this->params as $key => $param) {
+        foreach ($this->params as $key => $param) {
             $$key = $param;
         }
     }
