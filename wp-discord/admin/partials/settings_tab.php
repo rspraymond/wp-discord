190b367cc405ce0e51fcac7bee8cfa72e0cf208f
Raymond Perez <raymond.perez@schomp.com>
2016-09-19 20:11:25 -0600

    Settings tabs for post type templates.

--- wp-discord/admin/partials/settings_tab.php
+++ wp-discord/admin/partials/settings_tab.php
@@ -1,1 +1,40 @@
-<p>Believe in yourself. Well thats the place to start.</p>
+<h2>Discord Message Templates</h2>
+<div class="wpd-tabs">
+    <ul>
+    <?php
+        $args = array(
+            'public'   => true,
+        );
+
+        $post_types = get_post_types($args, 'objects');
+        $banned_types = ['attachment', 'nav_menu_item', 'revision'];
+
+        //cleanup
+        foreach($post_types as $key => $type) {
+            if (in_array($type->name, $banned_types)) {
+                unset($post_types[$key]);
+            }
+        }
+
+        //tab menu
+        foreach($post_types as $type) {
+            $slug = $type->name;
+            echo '<li><a href="#wpd_' . $slug . '" role="tab" id="tab_wpd_' . $slug . '" aria-controls="wpd_' . $slug . '">' . ucwords(str_replace(['-', '_'], ' ', $slug)) . '</a></li>';
+        }
+    ?>
+    </ul>
+
+    <?php
+        //tabs
+        foreach($post_types as $type)
+        {
+            $slug = $type->name;
+            ?>
+                <div id="wpd_<?php echo $slug; ?>">
+                    <?php echo ucwords(str_replace(['-', '_'], ' ', $slug)); ?>
+                </div>
+
+            <?php
+        }
+    ?>
+</div>
