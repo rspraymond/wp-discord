1f1e37f629701cfbc8aa3c487c3f8e260fc47cd8
Raymond Perez <raymond.perez@schomp.com>
2016-09-24 20:12:06 -0600

    Move tabs css to component.

---
+++ wp-discord/includes/helpers/class-wp-doin-shortcode.php
@@ -0,0 +1,155 @@
+<?php
+
+class WP_Doin_Shortcode {
+
+	public $fields = array();
+	public $id ='';
+	public $name = '';
+	public $description = '';
+
+	/**
+	 * Make sure each field is stored as a separate entity with unique variables passed
+	 * 
+	 * @param string $id
+	 * @param string $name
+	 * @param string $description
+	 */
+	public function __construct($id, $name, $description) {
+		$this->id = $id;
+		$this->name = $name;
+		$this->description = $description;
+	}
+
+	/**
+	 * Add a field to the object
+	 * 
+	 * @param string $type
+	 * @param string $name
+	 * @param string $heading
+	 * @param string $description
+	 * @param array $values
+	 * @return \WP_Doin_Shortcode
+	 */
+	public function add_field($type, $name, $heading = '', $description = '', $values = [ ]) {
+		$this->fields[$name] = array( 'type' => $type, 'heading' => $heading, 'description' => $description, 'values' => $values );
+		return $this;
+	}
+
+	/**
+	 * Iterate over the stored fields and generate their corresponding markup
+	 */
+	public function generate_fields() {
+		foreach ( $this->fields as $key => $field ) {
+			switch ( $field['type'] ) {
+				case 'col':
+					$this->generate_col( $field['heading'] );
+					break;
+				case 'text':
+					$this->generate_text( $key, $field['heading'], $field['description'] );
+					break;
+				case 'textarea':
+					$this->generate_textarea( $key, $field['heading'], $field['description'] );
+					break;
+				case 'checkbox':
+					$this->generate_checkbox( $key, $field['heading'], $field['description'], $field['values'] );
+					break;
+				case 'select':
+					$this->generate_select( $key, $field['heading'], $field['description'], $field['values'] );
+					break;
+				default:
+					$this->generate_text( $key, $field['heading'], $field['description'] );
+					break;
+			}
+		}
+	}
+
+	/**
+	 * Generate the 1/3 column markup for the TinyMCE popup
+	 * 
+	 * @param string $type either start or any (usually end :P)
+	 */
+	public function generate_col($type) {
+		if ( $type === 'start' ):
+			?>
+			<div class="columns-3"> 
+			<?php else: ?>
+			</div>
+		<?php
+		endif;
+	}
+
+	public function generate_text($key, $heading, $description) {
+		?>
+		<div class="field-container <?php echo $key; ?>">
+			<div class="label-desc">
+				<label for="<?php echo $heading; ?>"><?php echo $heading; ?></label>
+			</div>
+			<div class="content">
+				<input type="text" id="<?php echo $key; ?>"/>
+				<?php if ( !empty( $description ) ): ?>
+					<p><?php echo $description; ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+	}
+
+	public function generate_textarea($key, $heading, $description) {
+		?>
+		<div class="field-container <?php echo $key; ?>">
+			<div class="label-desc">
+				<label for="<?php echo $heading; ?>"><?php echo $heading; ?></label>
+			</div>
+			<div class="content">
+				<textarea id="<?php echo $key; ?>"/></textarea>
+				<?php if ( !empty( $description ) ): ?>
+					<p><?php echo $description; ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+	}
+
+	public function generate_select($key, $heading, $description, $values) {
+		?>
+		<div class="field-container <?php echo $key; ?>">
+			<div class="label-desc">
+				<label for="<?php echo $heading; ?>"><?php echo $heading; ?></label>
+			</div>
+			<div class="content">
+				<select id="<?php echo $key; ?>">
+					<?php foreach ( $values as $key => $value ): ?>
+						<option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+					<?php endforeach; ?>
+				</select>
+				<?php if ( !empty( $description ) ): ?>
+					<p><?php echo $description; ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+	}
+
+	public function generate_checkbox($key, $heading, $description, $values) {
+		?>
+		<div class="field-container <?php echo $key; ?>">
+			<div class="label-desc">
+				<label for="<?php echo $heading; ?>"><?php echo $heading; ?></label>
+			</div>
+			<div class="content">
+				<?php foreach ( $values as $key => $value ): 
+					?>
+					<input type="checkbox" id="<?php echo $key; ?>" />
+					<?php if ( !empty( $value ) ): ?>
+						<p><?php echo $value; ?></p>
+					<?php endif; ?>
+				<?php endforeach; ?>
+				<?php if ( !empty( $description ) ): ?>
+					<p><?php echo $description; ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+	}
+
+}
---
+++ wp-discord/includes/helpers/class-wp-doin-shortcodes-generator.php
@@ -0,0 +1,132 @@
+<?php
+
+include_once(plugin_dir_path(__FILE__) .  'shortcodes.php');
+
+Class WP_Doin_Shortcodes_Generator {
+
+	/**
+	 * Store the shortcodes created
+	 */
+	private $shortcodes = array();
+
+	/**
+	 * Genrate an array of all shortcodes added
+	 * 
+	 * @return \WP_Doin_Shortcodes_Generator
+	 */
+	public function add_shortcode($id, $name, $description) {
+		$shortcode = new WP_Doin_Shortcode( $id, $name, $description );
+		$this->shortcodes[$id] = $shortcode;
+		return $shortcode;
+	}
+
+	/**
+	 * Hook into the media buttons and admin footer to show up the thicbkox script and add media buttons
+	 */
+	public function generate() {
+		add_action( 'media_buttons', array( $this, 'wp_doin_media_buttons' ) );
+		add_action( 'admin_footer', array( $this, 'wp_doin_mce_popup' ) );
+	}
+
+	/**
+	 * Hook into the media buttons to show up custom media buttons
+	 * @hook admin_footer
+	 */
+	public function wp_doin_media_buttons() {
+		// iterate over all of the fields and introduce corresponding buttons
+		if ( !empty( $this->shortcodes ) ):
+			add_thickbox();
+			?>
+			<ul>
+				<li class="wp_doin_show_dropdown"><a href="#" class="button wp_doin_media_link">WP Doin Shortcodes</a>
+					<ul >
+						<?php foreach ( $this->shortcodes as $name => $field ):
+							?>
+							<li><a href = "#TB_inline?width=900&height=1200&inlineId=<?php echo $name; ?>" class = "button thickbox wp_doin_media_link"  title = "<?php echo $field->name; ?>"><?php echo $field->name; ?></a><p><em><?php echo $field->description; ?></em></p></li>
+						<?php endforeach; ?>
+					</ul>
+				</li>
+			</ul>
+		<?php endif; ?>
+		<?php
+	}
+
+	/**
+	 * Utility to add MCE Popup fired by custom Media Buttons button
+	 * 
+	 * @hook admin_footer
+	 */
+	public function wp_doin_mce_popup() {
+		?>
+
+		<script>
+			var obj = <?php echo json_encode( $this->shortcodes ); ?>;
+
+			function InsertShortcode(name) {
+				var atts = '';
+
+				jQuery.each(obj[name]['fields'], function (key, value) {
+					// get field type and specify different values for the atts as in textarea, select, checkbox, radio
+					if (value['type'] === 'text') {
+						var val = jQuery('.wp_doin_shortcode.' + name).find('.field-container.' + key).find('input').attr('value');
+						if (val !== '') {
+							atts += ' ' + key + '="' + val + '"';
+						}
+					}
+
+					if (value['type'] === 'textarea') {
+						var val = jQuery('.wp_doin_shortcode.' + name).find('.field-container.' + key).find('textarea').val();
+						if (val !== '') {
+							atts += ' ' + key + '="' + val + '"';
+						}
+					}
+
+					if (value['type'] === 'checkbox') {
+						var val = jQuery('.wp_doin_shortcode.' + name).find('.field-container.' + key).find('input:checked').val();
+						if (typeof val !== 'undefined') {
+							atts += ' '+ key + '="' + val + '"';
+						}
+					}
+
+					if (value['type'] === 'select') {
+						var val = jQuery('.wp_doin_shortcode.' + name).find('.field-container.' + key).find('select option:selected').val();
+						if (val !== '') {
+							atts += ' ' + key + '="' + val + '"';
+						}
+					}
+
+				});
+				window.send_to_editor('[' + name + atts + ']');
+			}
+		</script>
+
+		<?php 
+		/**
+		 * Iterate over all of the shortcodes created and construct the thickbox triggering buttons
+		 */
+		foreach ( $this->shortcodes as $name => $shortcode ) : ?>
+			<div id="<?php echo $name; ?>" style="display:none;">
+				<div class="wrap wp_doin_shortcode <?php echo $name; ?>">
+					<div>
+						<div style="overflow-y:scroll; height:650px;">
+							<h3 style="color:#5A5A5A!important; font-family:Georgia,Times New Roman,Times,serif!important; font-size:1.8em!important; font-weight:normal!important;"><?php echo $shortcode->name; ?></h3>
+							<p><?php echo $shortcode->description; ?></p>
+							<hr />
+							<?php echo $shortcode->generate_fields(); ?>
+						</div>
+
+						<hr />
+						<div style="padding:40px;">
+							<input type="button" class="button-primary" value="<?php echo __('Insert Shortcode', 'acf_rpw'); ?>" onclick="InsertShortcode('<?php echo $name; ?>');"/>&nbsp;&nbsp;&nbsp;
+							<a class="button" href="#" onclick="tb_remove();
+									return false;">Cancel</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endforeach; ?>
+
+		<?php
+	}
+
+}
